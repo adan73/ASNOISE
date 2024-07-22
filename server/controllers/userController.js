11eccf503a6b2d642daa32d77992e4f965f88631
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-22 20:44:58 +0300

    build a function that print or get the data for the user we ask about

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,8 +28,36 @@ const userController = {
             console.error('Error inserting user into the database:', err.message);
             res.status(500).json({ error: "Error inserting user into the database" });
         }
-    }
+    },
+    async getUser(req, res) {
+        const {email, user_password } = req.body;
     
+        if (!user_password || !email) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+        const connection = await dbConnection.createConnection();
+
+        try {
+            let [rows] = await connection.execute('SELECT * FROM dbShnkr24stud.tbl_121_users WHERE email = ?', [email]);
+        
+            if (rows.length === 0) {
+                return res.status(404).json({ error: "User not found" });
+            }
+    
+            const user = rows[0];
+    
+            if (user.user_password !== user_password) {
+                return res.status(401).json({ error: "Invalid password" });
+            }
+    
+            res.status(200).json(rows);
+        } catch (err) {
+            console.error('Error retrieving user from the database:', err.message);
+            res.status(500).json({ error: "Error retrieving data from the database" });
+        } finally {
+            connection.end();
+        }
+    }
 };
 
 
