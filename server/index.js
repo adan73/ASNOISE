613e132babbc96d544fd77accf1ed7d670752e5e
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-23 20:11:17 +0300

    change the path to /api/users

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.static('client'));
-app.use('/api', userRoutes);
+app.use('/api/users', userRoutes);
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.get('/test', (req, res) => {
