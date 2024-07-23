495402bc6a13fcb63069e79e19de3b1b3b2e2507
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-24 01:00:54 +0300

    add a function that fetch the patients from the db and printing it in the lest of the home page if the user is a doctor

--- client/Js/script.js
+++ client/Js/script.js
@@ -3,6 +3,52 @@ window.onload = () => {
     
     const userNameElement = document.getElementById('user-name');
     if (userNameElement) {
-        userNameElement.textContent = userName ? userName : 'Guest'; // Default to 'Guest' if no name is found
+        userNameElement.textContent = userName ? userName : 'Guest'; 
     }
+
+   
 };
+
+function PrintPatientsList() {
+    fetch("https://asnoise-4.onrender.com/api/patients/Allpatients")
+      .then((response) => response.json())
+      .then((data) => {
+        loadJSONData(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+  
+  function loadJSONData(data) {
+    const personInfo = document.getElementById("result");
+    personInfo.innerHTML = ''; 
+  
+    const ul = document.createElement("ul");
+  
+    data.patients.forEach((patient) => {
+      const li = document.createElement("li");
+      li.classList.add("patient-li");
+      const img = document.createElement("img");
+      img.classList.add("patione-img");
+      
+         
+      img.src =  patient.photo ;
+      img.alt = patient.first_name;
+
+      const patientName = document.createElement("div");
+      patientName.classList.add("patient-name");
+      patientName.textContent = `${patient.first_name} ${patient.last_name}`; 
+  
+      li.appendChild(img);
+      li.appendChild(patientName);
+  
+      li.onclick = () => {
+        window.sessionStorage.setItem("patientData", JSON.stringify(patient));
+        window.location.href = "patientPage.html";
+      };
+  
+      ul.appendChild(li);
+    });
+  
+    personInfo.appendChild(ul);
+  }
+  
