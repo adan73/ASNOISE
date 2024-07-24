c70e9d918e1e44bda6d6fc04b9461c53370f85eb
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-24 11:55:20 +0300

    fix the route

--- server/routes/activityRoutes.js
+++ server/routes/activityRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { activityController } = require('../controllers/activityController'); // Ensure this path is correct
 const activity_router = express.Router();
 
-activity_router.post('/addActivity', activityController.addPatient);
-activity_router.post('/getActivity', activityController.getPatients);
+activity_router.post('/addActivity', activityController.addActivity);
+activity_router.get('/getActivity', activityController.getActivity);
 
 module.exports = activity_router;
