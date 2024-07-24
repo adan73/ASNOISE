043df39e9b1e4c4d0134144ceb33e3b75dff7278
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-24 16:56:19 +0300

    add a new route for the function that print depend on the date

--- server/routes/activityRoutes.js
+++ server/routes/activityRoutes.js
@@ -3,6 +3,7 @@ const { activityController } = require('../controllers/activityController');
 const activity_router = express.Router();
 
 activity_router.post('/addActivity', activityController.addActivity);
-activity_router.get('/getActivity', activityController.getActivity);
+activity_router.get('/getAllActivity', activityController.getAllActivity);
+activity_router.post('/getDateActivity', activityController.getActivityFor_Date);
 
 module.exports = activity_router;
