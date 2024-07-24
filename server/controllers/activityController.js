f052266c3965f3e327cda836bebb7e4197d351e1
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-24 11:26:11 +0300

    add a new controller of the user activity and inside it 2 function one to add a new activity in the db and anther to get the activity depend in the username

---
+++ server/controllers/activityController.js
@@ -0,0 +1,73 @@
+
+const { dbConnection } = require("../db_connection");
+
+const activityController = {
+    async addActivity(req, res) {
+      const {username ,time ,the_activity } = req.body;
+  
+      if (!the_activity ||!time ||!username ) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+      const connection = await dbConnection.createConnection();
+  
+      try {
+        let [rows1] = await connection.execute(
+          `SELECT * FROM dbShnkr24stud.tbl_121_user_activity WHERE time = '${time}'`);
+        if (rows1.length > 0) {
+          return res.status(400).json({ error: "There is an activity in this time already" });
+        }
+        let [rows2] = await connection.execute(
+          `SELECT * FROM dbShnkr24stud.tbl_121_user_activity WHERE the_activity = '${the_activity}'`);
+        if (rows2.length > 0) {
+          return res.status(400).json({ error: "This activity already exist" });
+        }
+  
+        const [result] = await connection.execute(
+          `INSERT INTO dbShnkr24stud.tbl_121_user_activity (username ,time ,the_activity ) 
+          VALUES ( '${username}' ,  '${time}' ,  '${the_activity}')`);
+  
+        if (result.affectedRows > 0) {
+          res
+            .status(200)
+            .json({ success: true, message: "Activity added successfully" });
+        } else {
+          res.status(500).json({ error: "Failed to add Activity" });
+        }
+      } catch (err) {
+        console.error("Error inserting user into the database:", err.message);
+        res.status(500).json({ error: "Error inserting user into the database" });
+      } finally {
+        await connection.end();
+      }
+    },
+    async getActivity(req, res) {
+      const { username } = req.body;
+  
+      if ( !username) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+      const connection = await dbConnection.createConnection();
+  
+      try {
+        let [rows] = await connection.execute(
+          `SELECT * FROM dbShnkr24stud.tbl_121_user_activity WHERE username = '${username}'`
+        );
+  
+        if (rows.length === 0) {
+          return res.status(404).json({ error: "User doesn't have a activities " });
+        }
+        res.status(200).json(rows);
+        
+      } catch (err) {
+        console.error("Error retrieving user from the database:", err.message);
+        res
+          .status(500)
+          .json({ error: "Error retrieving data from the database" });
+      } finally {
+        connection.end();
+      }
+    }
+
+};
+
+module.exports = { activityController };
