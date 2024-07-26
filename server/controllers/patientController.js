4f0b2309710b463280a5e1489a77393ee33d7343
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-26 12:17:09 +0300

    .

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -53,14 +53,14 @@ const patientController = {
     }
   },
   async  getDoctorPatients(req, res) {
-    const{first_name,photo} = req.body;
+    const{doctor,doctor_photo} = req.body;
     
     if (!first_name || !photo) {
       return res.status(400).json({ error: "Missing required fields" });
     }
     const connection = await dbConnection.createConnection();
     try {
-      const [rows] = await connection.execute( `SELECT * FROM dbShnkr24stud.tbl_121_patients WHERE doctor = '${first_name}' AND doctor_photo = '${photo}' `); 
+      const [rows] = await connection.execute( `SELECT * FROM dbShnkr24stud.tbl_121_patients WHERE doctor = '${doctor}' AND doctor_photo = '${doctor_photo}' `); 
       if (rows.length === 0) {
         return res.status(400).json({ error: "There are no patients" });
       }
