e1ebf6a076b7595b3e0cc7f0fe9a75384447c301
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-26 12:01:55 +0300

    fix

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -4,7 +4,7 @@ const patient_router = express.Router();
 
 patient_router.post('/addPatient', patientController.addPatient);
 patient_router.get('/Allpatients', patientController.getPatients);
-patient_router.get('/:first_name,photo', patientController.getDoctorPatients);
+patient_router.get('/:first_name/:photo', patientController.getDoctorPatients);
 patient_router.get('/:patient_id', patientController.getDoctor);
 patient_router.delete('/:patient_id', patientController.deletePatient);
 patient_router.put('/', patientController.updatePatient);
