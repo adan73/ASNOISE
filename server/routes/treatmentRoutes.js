f5a4b64db00957a6ec8e77fbdc429f9c7394e9fa
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-28 22:01:43 +0300

    fix the route for the addMethode

--- server/routes/treatmentRoutes.js
+++ server/routes/treatmentRoutes.js
@@ -4,7 +4,7 @@ const treatment_router = express.Router();
 
 
 treatment_router.get('/:patient_id', TreatmentMethodeController.getTreatment); 
-treatment_router.post('/:patient_id/:method', TreatmentMethodeController.addTreatment); 
+treatment_router.post('/addMethod', TreatmentMethodeController.addTreatment); 
 
 
 module.exports = treatment_router;
