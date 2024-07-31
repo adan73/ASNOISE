bd61e45d99a70f75f147b3477d5790da91eee8fa
adan73 <167682529+adan73@users.noreply.github.com>
2024-07-31 14:52:04 +0300

    change from post to get

--- server/routes/activityRoutes.js
+++ server/routes/activityRoutes.js
@@ -4,6 +4,6 @@ const activity_router = express.Router();
 
 activity_router.post('/addActivity', activityController.addActivity);
 activity_router.get('/', activityController.getAllActivity);
-activity_router.post('/:username /:date', activityController.getActivityFor_Date);
+activity_router.get('/:username /:date', activityController.getActivityFor_Date);
 
 module.exports = activity_router;
