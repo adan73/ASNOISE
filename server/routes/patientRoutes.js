d550bb75998dea8b5af1787745c0bef5dacc13d8
adan73 <167682529+adan73@users.noreply.github.com>
2024-08-01 15:58:11 +0300

    removed names in the route and let it be just /

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { patientController } = require('../controllers/patientController'); // Ensure this path is correct
 const patient_router = express.Router();
 
-patient_router.post('/addPatient', patientController.addPatient);
-patient_router.get('/Allpatients', patientController.getPatients);
+patient_router.post('/', patientController.addPatient);
+patient_router.get('/', patientController.getPatients);
 patient_router.get('/:doctor/:doctor_photo', patientController.getDoctorPatients);
 patient_router.get('/:patient_id', patientController.getDoctor);
 patient_router.delete('/:patient_id', patientController.deletePatient);
